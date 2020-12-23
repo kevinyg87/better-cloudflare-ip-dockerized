cd558a44e2629e84a797dc3d906f87b91c6d134d
silencebay <silencebay@users.noreply.github.com>
2020-12-24 00:46:13 +0800

    added default CRON

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 # remove first and last quote (")
 CRON="${CRON%\"}"
 CRON="${CRON#\"}"
+[ -z "$CRON" ] && CRON="0 */6 * * *"
 
 echo "${CRON} /usr/bin/flock -n /tmp/fcj.lockfile /usr/local/bin/cf.sh > /proc/1/fd/1 2>/proc/1/fd/2" > /etc/crontabs/root;
 
